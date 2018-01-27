990f1cd433a4fffb30848854c9c33831614a987a
John Alexander <jbalexander88@gmail.com>
2018-01-26 16:26:32 -0800

    iterative mnist neural network

--- mnist.py
+++ mnist.py
@@ -2,17 +2,9 @@ import tensorflow as tf
 import os
 
 
-LOGDIR = "/Users/john/Documents/MNIST/"
-# LABELS = os.path.join(os.getcwd(), "labels_1024.tsv")
-# SPRITES = os.path.join(os.getcwd(), "sprite_1024.png")
-
+LOGDIR = "C:/Users/johna/Documents/Deep Learning/mnist/"
+SAVEDIR = LOGDIR + '/model_saves/'
 mnist = tf.contrib.learn.datasets.mnist.read_data_sets(train_dir=LOGDIR + "data", one_hot=True)
-
-# if not (os.path.isfile(LABELS) and os.path.isfile(SPRITES)):
-#   print("Necessary data files were not found. Run this command from inside the "
-#     "repo provided at "
-#     "https://github.com/dandelionmane/tf-dev-summit-tensorboard-tutorial.")
-#   exit(1)
 
 def weight_variable(shape):
     weight = tf.truncated_normal(shape, stddev=0.1)
@@ -22,26 +14,59 @@ def bias_variable(shape):
     bias = tf.constant(0.1, shape=shape)
     return tf.Variable(bias)
 
-def neural_network(input, num_input, num_layers, num_neurons, num_classes):
-
+def neural_network(input, num_input, num_layers, num_neurons, num_classes, activation_function):
     input_channel = num_input
     output_channel = num_neurons
     prev_layer = input
 
     if num_layers > 1:
         for _ in range(num_layers):
-            out_layer = tf.add(tf.matmul(
-                prev_layer, weight_variable([input_channel, output_channel])),
-                bias_variable([output_channel]))
+            if activation_function == 'relu':
+                out_layer = tf.nn.relu(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            elif activation_function == 'sigmoid':
+                out_layer = tf.nn.sigmoid(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            elif activation_function == 'tanh':
+                out_layer = tf.nn.tanh(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            else:
+                out_layer = tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel]))
+
 
             prev_layer = out_layer
             input_channel = output_channel
 
-    final_layer = tf.add(tf.matmul(
-        prev_layer, weight_variable([input_channel, num_classes])),
-        bias_variable([num_classes]))
+    if activation_function == 'relu':
+        final_layer = tf.nn.relu(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    elif activation_function == 'sigmoid':
+        final_layer = tf.nn.sigmoid(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    elif activation_function == 'tanh':
+        final_layer = tf.nn.tanh(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    else:
+        final_layer = tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes]))
 
     return final_layer
+
+def save_model(hparam, save_step, sess, saver):
+    save_path = (SAVEDIR + hparam)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    saver.save(sess, (save_path + '/model.ckpt'), global_step=save_step)
+    print('Model saved to %s' % save_path)
 
 def mnist_model(learning_rate,
                 batch_size,
@@ -51,7 +76,9 @@ def mnist_model(learning_rate,
                 num_layers,
                 num_input,
                 num_classes,
-                hparam):
+                hparam,
+                save_step,
+                activation_function):
 
     # Reset the graph
     tf.reset_default_graph()
@@ -62,7 +89,7 @@ def mnist_model(learning_rate,
     y_ = tf.placeholder(tf.float32, shape=[None, num_classes])
 
     # Output of network
-    y = neural_network(x, num_input, num_layers, num_neurons, num_classes)
+    y = neural_network(x, num_input, num_layers, num_neurons, num_classes, activation_function)
 
     # Cost function
     with tf.name_scope('cross_entropy'):
@@ -90,7 +117,7 @@ def mnist_model(learning_rate,
 
     # Initialize all variables
     sess.run(tf.global_variables_initializer())
-    writer = tf.summary.FileWriter(LOGDIR + hparam)
+    writer = tf.summary.FileWriter(LOGDIR + '/model_summaries/'+ hparam)
     writer.add_graph(sess.graph)
 
     # Train
@@ -102,44 +129,53 @@ def mnist_model(learning_rate,
             [train_accuracy, sum] = sess.run([accuracy, summary], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
             print('training accuracy: %s, step: %d'%(train_accuracy, step))
             writer.add_summary(sum, step)
+        if step % save_step == 0:
+            save_model(hparam, step, sess, saver)
 
         # Training step
         sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
 
-def make_hparam_string(learning_rate, num_neurons, num_layers):
-    return "lr_%.0E,neurons=%s,layers=%s" % (learning_rate, num_neurons, num_layers)
+def make_hparam_string(learning_rate, num_neurons, num_layers, activation_function):
+    return "lr_%.0E_neurons=%s_layers=%s_afunc=%s" % (learning_rate, num_neurons, num_layers, activation_function)
 
 def main():
     # Parameters
-    learning_rate = 0.1
     batch_size = 50
-    num_epochs = 5000
+    num_epochs = 20000
     display_step = 100
+    save_step = 2500
 
     # Network Parameters
-    num_neurons = 50  # number of neurons in each layer
-    num_layers = 2  # number of layers
     num_input = 784  # MNIST data input (img shape: 28*28)
     num_classes = 10  # MNIST total classes (0-9 digits)
 
-    hparam = make_hparam_string(learning_rate, num_neurons, num_layers)
-    print('Starting run for %s'%hparam)
+    # Parameter search
+    num_of_neurons = [10,20,50]
+    num_of_layers = [1,2,5]
+    learning_rates = [0.1,0.3,0.5]
+    activation_functions = ['relu','sigmoid','tanh']
 
-    mnist_model(learning_rate,
-                batch_size,
-                num_epochs,
-                display_step,
-                num_neurons,
-                num_layers,
-                num_input,
-                num_classes,
-                hparam)
+    for learning_rate in learning_rates:
+        for neurons in num_of_neurons:
+            for layers in num_of_layers:
+                for activation_function in activation_functions:
+                    hparam = make_hparam_string(learning_rate, neurons, layers, activation_function)
+                    print('Starting run for %s'%hparam)
+
+                    mnist_model(learning_rate,
+                                batch_size,
+                                num_epochs,
+                                display_step,
+                                neurons,
+                                layers,
+                                num_input,
+                                num_classes,
+                                hparam,
+                                save_step,
+                                activation_function)
 
     print('Done training!')
     print('Run `tensorboard --logdir=%s` to see the results.' % LOGDIR)
-    print('Running on mac? If you want to get rid of the dialogue asking to give '
-          'network permissions to TensorBoard, you can provide this flag: '
-          '--host=localhost')
 
 if __name__ == '__main__':
     main()
